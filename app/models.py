daadfdc3c164e941d478fd31cb7e926353703003
Doktatech <Doktatech2@gmail.com>
2019-07-17 18:17:09 +0300

    Create category model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=100,blank=False)
+    def __str__(self):
+        return self.category
+
 class Product(models.Model):
     name = models.CharField(max_length= 50,blank=False,null= False)
     description = models.CharField(max_length=200)
@@ -11,19 +18,13 @@ class Product(models.Model):
     quantity = models.IntegerField(default=0)
     size = models.IntegerField(unique= True)
     category=models.ForeignKey(Category,on_delete=models.CASCADE) 
-       def __str__(self):
+    def __str__(self):
         return self.name
 
 class Warehouse(models.Model):
-    locality=models.CharField(max_length=100,default=Karen)
+    locality=models.CharField(max_length=100,default='Karen')
     def __str__(self):
         return self.locality
-
-
-class Category(models.Model):
-    category = models.CharField(max_length=100,blank=False)
-    def __str__(self):
-        return self.category
 
 class OrderDetails(models.Model):
     order_id=models.IntegerField(primary_key=True)
@@ -32,6 +33,10 @@ class OrderDetails(models.Model):
     date= models.DateTimeField(auto_now=True)
     product=models.ForeignKey(Product,on_delete=models.CASCADE,blank=False,null=False)
     def __str__(self):
-        return self.order_id
+        return str(self.order_id)
+    @classmethod
+    def search_by_id(cls,search_id):
+        product=cls.ohjects.filter(order_id__icontains=search_id)
+        return 
 
 
