f8009171bf66d5006d2f070eb9bd8f2dc81b02e4
Brightmuk <be.rightmuk@gmail.com>
2019-07-18 16:09:41 +0300

    Add function to change quantity of stock items on order

--- app/views.py
+++ app/views.py
@@ -1,33 +1,68 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import *
 from .forms import *
 
-
-def news_today(request):
-   houses = Warehouse.objects.all()
+'''
+View for the inventory page displaying categories
+of items in stock
+'''
+def inventory(request):
+   houses = Distributor.objects.all()
    objects = Product.objects.all()
 
 
    return render(request, 'vendors.html', {'objects':objects,'houses':houses})
-def house(request,id):
 
-   house = Warehouse.objects.get(id=id)
-   house_orders=OrderDetails.objects.filter(warehouse=id)
+'''
+View for a single distributor
+'''
+def single_house(request,id):
 
-   if request.method == 'POST':
-       form = NewProd(request.POST, request.FILES)
-       if form.is_valid():
-           item = form.save(commit=False)
-           item.warehouse=house
-           prod=Product.objects.get(id=item.product.id)
-           prod.quantity = prod.quantity-item.quantity
-           print(prod.quantity)
-           prod.save()
-           item.save()
-           return redirect(news_today)
-   else:
-       form =NewProd()
-   return render(request,'house.html',{'house':house,'form':form,'orders':house_orders})
+   house = House.objects.get(id=id)
+    house_products = House_Product.objects.filter(warehouse=id)
+    if request.method == 'POST':
+        form = NewHouseProd(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.warehouse=house
+            item.save()
+            return redirect(news_today)
+    else:
+        form =NewHouseProd()
+    return render(request,'house.html',{'house':house,'form':form,'products':house_products})
+'''
+view for a single item within a distributor 
+'''
+
+
+def add_item(request,h_id,i_id):
+    house = House.objects.get(id=h_id)
+    product = MoringaMerch.objects.get(id=i_id)
+    to_update = House_Product.objects.get(name=product) 
+    if request.method == 'POST':
+        form = NewProd(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.product = product
+            item.warehouse=house
+            prod=MoringaMerch.objects.get(id=item.product.id)
+            prod.quantity = prod.quantity-item.quantity
+            prod_add = House_Product.objects.get(name=prod)
+            prod_add.quantity=prod_add.quantity+item.quantity
+            item.save()
+            prod_add.save()
+            prod.save()
+            return redirect(news_today)
+    else:
+        form =NewProd()
+    return render(request,'item.html',{'product':product,'house':house,'to_update':to_update,'form':form})
+
+
 
 def search(request):
     if 'product' in request.GET and request.GET['product']:
