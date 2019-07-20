ab4d14d429f319dfdbb505a703df06d208b897a4
Brightmuk <be.rightmuk@gmail.com>
2019-07-20 14:18:45 +0300

    Create urls for linking views to test templates

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
- url(r'add/(\d+)/(\d+)',views.add_item,name="add"),
- url(r'house/(\d+)',views.single_house,name="house")  
+ url(r'^$',views.inventory,name='inventory'),
+ url(r'^search/',views.search,name='search_results'),
+ url(r'^category/(\d+)$',views.category,name='category'),
+ url(r'^house/(\d+)$',views.single_house,name="house"),
+ url(r'^house/update(\d+)/(\d+)$',views.add_house_product,name="add_product"),
+ url(r'^category/product/(\d+)$',views.stock_product,name="stock_product"),  
 ]
+
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
