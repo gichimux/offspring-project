97c99860cfb869fc354731a4ce0f45c99ff091d6
Brightmuk <be.rightmuk@gmail.com>
2019-07-20 22:42:26 +0300

    Register new models to admin

--- app/admin.py
+++ app/admin.py
@@ -6,6 +6,9 @@ admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Distributor)
 admin.site.register(OrderDetails)
+admin.site.register(House_Product)
+admin.site.register(Supplier)
+admin.site.register(Order_Product)
 
 admin.site.site_header = "Offspring";
 admin.site.site_title = "Offspring Inventory ";
