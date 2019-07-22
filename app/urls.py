111164527ab73412917eccd61de16ca7f9acc0b3
Brightmuk <be.rightmuk@gmail.com>
2019-07-22 08:48:28 +0300

    Create the  url mappings for different house locations

--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,11 @@ urlpatterns=[
  url(r'^$',views.inventory,name='inventory'),
  url(r'^search/',views.search,name='search_results'),
  url(r'^category/(\d+)$',views.category,name='category'),
- url(r'^house/(\d+)$',views.single_house,name="house"),
- url(r'^house/update(\d+)/(\d+)$',views.add_house_product,name="add_product"),
- url(r'^category/product/(\d+)$',views.stock_product,name="stock_product"),  
+ url(r'^category/product/(\d+)$',views.stock_product,name="stock_product"),
+
+ url(r'^house/(\d+)$',views.single_house,name="single_house"),
+ url(r'^house/category/(\d+)/(\d+)$',views.house_category,name="house_category"),
+ url(r'^house/category/product/(\d+)/(\d+)$',views.add_house_product,name="add_house_product"),
 
  url(r'^analysis$',views.full_stock,name='analysis'),
  url(r'^analysis/category/(\d+)$',views.full_category,name='category_analysis'),
