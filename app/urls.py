cd76042d8ea0df70ce4c11a66cef19bafb6c0402
Brightmuk <be.rightmuk@gmail.com>
2019-07-23 14:10:49 +0300

    Add urls to the suppiers fucntion

--- app/urls.py
+++ app/urls.py
@@ -7,13 +7,16 @@ from django.conf.urls.static import static
 urlpatterns=[
  url(r'^$',views.inventory,name='inventory'),
  url(r'^search/',views.search,name='search_results'),
- url(r'^category/(\d+)$',views.category,name='category'),
+ url(r'^category/(\d+)$',views.stock_category,name='category'),
  url(r'^category/product/(\d+)$',views.stock_product,name="stock_product"),
 
  url(r'^house/(\d+)$',views.single_house,name="single_house"),
  url(r'^house/category/(\d+)/(\d+)$',views.house_category,name="house_category"),
  url(r'^house/category/product/(\d+)/(\d+)$',views.add_house_product,name="add_house_product"),
 
+ url(r'^supplier$',views.suppliers,name="suppliers"),
+ url(r'^supplier/(\d+)$',views.single_supplier,name="single_supplier"),
+     
  url(r'^analysis$',views.full_stock,name='analysis'),
  url(r'^analysis/category/(\d+)$',views.full_category,name='category_analysis'),
  url(r'^analysis/category/product/(\d+)$',views.product_analysis,name="stock_product_analysis"),  
